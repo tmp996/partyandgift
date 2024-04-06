a541b2402995e3b6c39b8564f91652e5e56dcd91
Angel Valladares <arivalladares3.0@outlook.es>
2024-04-06 14:22:43 -0600

    test

--- server/utils/database.js
+++ server/utils/database.js
@@ -10,9 +10,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
-  // ssl: {
-  //   rejectUnauthorized: true
-  // }
+  ssl: {
+    rejectUnauthorized: true
+  }
 });
 
 // Prueba de conexión directa
