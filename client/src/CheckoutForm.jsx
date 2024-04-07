912c45d56a39dfd24aba2e6f758816bd353948c5
Angel Valladares <arivalladares3.0@outlook.es>
2024-04-07 00:40:19 -0600

    update stripe

--- client/src/CheckoutForm.jsx
+++ client/src/CheckoutForm.jsx
@@ -1,3 +1,5 @@
+// CheckoutForm.jsx
+
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
 
 const CheckoutForm = ({ total, onSuccess }) => {
@@ -7,27 +9,29 @@ const CheckoutForm = ({ total, onSuccess }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    if (elements == null) {
+    if (!stripe || !elements) {
+      // Stripe.js aún no se ha cargado
       return;
     }
 
+    const cardElement = elements.getElement(CardElement);
+
     const { error, paymentMethod } = await stripe.createPaymentMethod({
       type: 'card',
-      card: elements.getElement(CardElement),
+      card: cardElement,
     });
 
     if (error) {
-      console.log('[error]', error);
+      console.error('[error]', error);
     } else {
-      console.log('[PaymentMethod]', paymentMethod);
-      onSuccess(paymentMethod.id);
+      onSuccess(paymentMethod.id); // Llamada a tu función onSuccess
     }
   };
 
   return (
     <form onSubmit={handleSubmit}>
       <CardElement />
-      <button type="submit" disabled={!stripe || !elements}>
+      <button type="submit" disabled={!stripe || !elements} className="stripe-submit-btn">
         Pagar L. {total}
       </button>
     </form>
